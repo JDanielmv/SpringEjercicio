22b887835f0c99900da8673f85c7d6b1d879bbde
JDanielmv <yanda.mv98@gmail.com>
2022-02-11 12:16:25 +0100

    primer commit

---
+++ api-rest-spring-ejercicio/src/main/java/com/formacionspring/app/apirest/ApiRestSpringEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.formacionspring.app.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringEjercicioApplication.class, args);
+	}
+
+}
