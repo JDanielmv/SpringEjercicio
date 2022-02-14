e83ed521b27c25152beea275eb8197ecb2477ff3
Hoennwalker <garciablazquezhector@gmail.com>
2022-02-14 12:39:43 +0100

    cambios hector

--- api-rest-spring-ejercicio/src/main/java/com/formacionspring/app/apirest/entity/Cliente.java
+++ api-rest-spring-ejercicio/src/main/java/com/formacionspring/app/apirest/entity/Cliente.java
@@ -5,13 +5,19 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+//import com.example.formacionspring.apirest.entity.Region;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="clientes")
@@ -32,9 +38,9 @@ public class Cliente implements Serializable{
 	@Column(name="fecha_nacimiento")
 	@Temporal(TemporalType.DATE)
 	private Date fechaNacimiento;
-	
-	
-	
+
+
+
 	@PrePersist
 	public void prePersist() {
 		if(fechaNacimiento == null) {
